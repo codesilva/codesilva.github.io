acf4da5bae0f9eff4dacf92c440fe65f58cbd91a
Edy Silva <edigleyssonsilva@gmail.com>
2025-09-25 11:29:31 -0300

    doing rotations with CSS

--- interviews/shopify/grid-challenge/src/components/Grid.jsx
+++ interviews/shopify/grid-challenge/src/components/Grid.jsx
@@ -4,12 +4,13 @@ import { Row } from './Row';
 import { Cell } from './Cell';
 import './Grid.css';
 
-const iconDirectionMap = {
-  'up': '👆',
-  'down': '👇',
-  'left': '👈',
-  'right': '👉',
-};
+const ICON = '👆';
+// const iconDirectionMap = {
+//   'up': '👆',
+//   'down': '👇',
+//   'left': '👈',
+//   'right': '👉',
+// };
 
 const headingIndex = ['up', 'right', 'down', 'left'];
 const GRID_SIZE = 10;
@@ -33,14 +34,14 @@ const stateTransitions = {
 };
 
 export function Grid() {
-  const [position, setPosition] = useState({ coords: { x: 0, y: 0 }, heading: 'right' });
+  const [position, setPosition] = useState({ coords: { x: 0, y: 0 }, heading: 'up' });
 
   const moveForward = () => {
     const { coords } = stateTransitions[position.heading](position);
     const crossedX = coords.x < 0 || coords.x >= GRID_SIZE;
     const crossedY = coords.y < 0 || coords.y >= GRID_SIZE;
-    const shouldTurnRight = crossedX || crossedY;
-    const heading = shouldTurnRight ? calculateNextHeading(position.heading) : position.heading;
+    const crossedBoard = crossedX || crossedY;
+    const heading = crossedBoard ? calculateNextHeading(position.heading) : position.heading;
 
     setPosition({
       coords: {
@@ -67,7 +68,9 @@ export function Grid() {
         <Row key={i}>
           {Array.from({ length: GRID_SIZE }).map((_, j) => (
             <Cell key={j}>
-              {position.coords.x === j && position.coords.y === i ? iconDirectionMap[position.heading] : null}
+              {/*position.coords.x === j && position.coords.y === i ? <span className={ `roomba ${position.heading}` }>{ iconDirectionMap[position.heading] }</span> : null*/}
+
+              {position.coords.x === j && position.coords.y === i ? <span className={ `roomba ${position.heading}` }>{ ICON }</span> : null}
             </Cell>
           ))}
         </Row>
