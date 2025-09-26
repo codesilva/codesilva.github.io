b93f4894a285e8be21e78170be90d79ffe3e10d7
Edy Silva <edigleyssonsilva@gmail.com>
2025-09-26 12:00:19 -0300

    simplify solution

--- interviews/shopify/grid-challenge/src/components/Grid.jsx
+++ interviews/shopify/grid-challenge/src/components/Grid.jsx
@@ -1,47 +1,32 @@
 import { useState } from 'react';
-
-import { Row } from './Row';
-import { Cell } from './Cell';
 import './Grid.css';
 
 const ICON = '👆';
-// const iconDirectionMap = {
-//   'up': '👆',
-//   'down': '👇',
-//   'left': '👈',
-//   'right': '👉',
-// };
-
 const headingIndex = ['up', 'right', 'down', 'left'];
 const GRID_SIZE = 10;
-const calculateNextHeading = (currentHeading) => {
+const TRANSITIONS = {
+  'up': { x: 0, y: -1 },
+  'right': { x: 1, y: 0 },
+  'down': { x: 0, y: 1 },
+  'left': { x: -1, y: 0 },
+}
+const getNextHeading = (currentHeading) => {
   const nextHeadingIndex = (headingIndex.indexOf(currentHeading) + 1) % headingIndex.length;
   return headingIndex[nextHeadingIndex];
-};
-const stateTransitions = {
-  'up': ({ coords: { x, y } }) => {
-    return { coords: { x, y: y - 1 } };
-  },
-  'right': ({ coords: { x, y } }) => {
-    return { coords: { x: x + 1, y } };
-  },
-  'down': ({ coords: { x, y } }) => {
-    return { coords: { x, y: y + 1 } };
-  },
-  'left': ({ coords: { x, y } }) => {
-    return { coords: { x: x - 1, y } };
-  },
 };
 
 export function Grid() {
   const [position, setPosition] = useState({ coords: { x: 0, y: 0 }, heading: 'up' });
 
   const moveForward = () => {
-    const { coords } = stateTransitions[position.heading](position);
+    const coords = {
+      x: position.coords.x + TRANSITIONS[position.heading].x,
+      y: position.coords.y + TRANSITIONS[position.heading].y,
+    };
     const crossedX = coords.x < 0 || coords.x >= GRID_SIZE;
     const crossedY = coords.y < 0 || coords.y >= GRID_SIZE;
     const crossedBoard = crossedX || crossedY;
-    const heading = crossedBoard ? calculateNextHeading(position.heading) : position.heading;
+    const heading = crossedBoard ? getNextHeading(position.heading) : position.heading;
 
     setPosition({
       coords: {
@@ -55,7 +40,7 @@ export function Grid() {
   const rotate90Deg = () => {
     setPosition({
       ...position,
-      heading: calculateNextHeading(position.heading)
+      heading: getNextHeading(position.heading)
     });
   };
 
@@ -65,15 +50,13 @@ export function Grid() {
 
     <div className='grid'>
       {Array.from({ length: GRID_SIZE }).map((_, i) => (
-        <Row key={i}>
+        <div className='row' key={i}>
           {Array.from({ length: GRID_SIZE }).map((_, j) => (
-            <Cell key={j}>
-              {/*position.coords.x === j && position.coords.y === i ? <span className={ `roomba ${position.heading}` }>{ iconDirectionMap[position.heading] }</span> : null*/}
-
+            <div className='cell' key={j}>
               {position.coords.x === j && position.coords.y === i ? <span className={ `roomba ${position.heading}` }>{ ICON }</span> : null}
-            </Cell>
+            </div>
           ))}
-        </Row>
+        </div>
       ))}
     </div>
   </>;
